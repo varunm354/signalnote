75fd2a9174a0d107e5564ea62aee3c8cb39f5bc3
Varun Mohanraj <reachvarunm@gmail.com>
2026-04-23 17:23:42 -0700

    enable vercel CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://signalnote-rose.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
