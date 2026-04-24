50269a68cda7315ce19b7faf549bd2976226a4b3
Varun Mohanraj <reachvarunm@gmail.com>
2026-04-23 17:31:30 -0700

    fix CORS to allow vercel deployment domains

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://signalnote-rose.vercel.app"
+        "https://signalnote-rose.vercel.app",
+        "https://signalnote-dyyf4oa87-varunm354s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
